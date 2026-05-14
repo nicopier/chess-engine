afe1d37435546a80e91f123863a5fab187b5b8c2
Nicopier <piersanti.nicolas@gmail.com>
2026-05-14 16:44:55 -0300

    setup all pieces on board

--- core/domain/board.py
+++ core/domain/board.py
@@ -1,4 +1,4 @@
-from .pieces import Pawn, Color
+from .pieces import Pawn, Color, Rook, Knight, Bishop, Queen, King
 
 class Board():
     def __init__(self):
@@ -13,6 +13,26 @@ class Board():
         for col in range(8):
             self.board[1][col] = Pawn(Color.WHITE, (1, col))
             self.board[6][col] = Pawn(Color.BLACK, (6, col))
+            # Blancas - fila 0
+        self.board[0][0] = Rook(Color.WHITE, (0, 0))
+        self.board[0][1] = Knight(Color.WHITE, (0, 1))
+        self.board[0][2] = Bishop(Color.WHITE, (0, 2))
+        self.board[0][3] = Queen(Color.WHITE, (0, 3))
+        self.board[0][4] = King(Color.WHITE, (0, 4))
+        self.board[0][5] = Bishop(Color.WHITE, (0, 5))
+        self.board[0][6] = Knight(Color.WHITE, (0, 6))
+        self.board[0][7] = Rook(Color.WHITE, (0, 7))
+        
+        # Negras - fila 7
+        self.board[7][0] = Rook(Color.BLACK, (7, 0))
+        self.board[7][1] = Knight(Color.BLACK, (7, 1))
+        self.board[7][2] = Bishop(Color.BLACK, (7, 2))
+        self.board[7][3] = Queen(Color.BLACK, (7, 3))
+        self.board[7][4] = King(Color.BLACK, (7, 4))
+        self.board[7][5] = Bishop(Color.BLACK, (7, 5))
+        self.board[7][6] = Knight(Color.BLACK, (7, 6))
+        self.board[7][7] = Rook(Color.BLACK, (7, 7))    
+                
             
     def get_piece_at(self, position:tuple[int, int]):
         row, col = position
