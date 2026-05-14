fce726ee4a76a66d7f4d83272f542f372e26e7f7
Nicopier <piersanti.nicolas@gmail.com>
2026-05-14 15:24:08 -0300

    setup inicial

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+# 1. Crear el entorno virtual
+python -m venv core/venv
+# 2. Activar el entorno virtual
+& core/venv/Scripts/Activate.ps1
+# 3. Instalar dependencias
+pip install -r core/requirements.txt
